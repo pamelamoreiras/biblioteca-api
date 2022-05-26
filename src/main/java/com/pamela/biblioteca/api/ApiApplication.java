18bb75dabca99a8a61aeafe1dae41a5622b7b2c0
pamelamoreiras <99215302+pamelamoreiras@users.noreply.github.com>
2022-05-26 18:31:01 -0300

    Update ApiApplication.java

--- src/main/java/com/pamela/biblioteca/api/ApiApplication.java
+++ src/main/java/com/pamela/biblioteca/api/ApiApplication.java
@@ -9,9 +9,4 @@ public class ApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
-
-	public void teste(){
-		System.out.println("Olá Mundo");
-	}
-
 }
