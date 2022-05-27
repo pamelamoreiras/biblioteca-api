948d39fced6091ffa7f41df963d22c59e4098b2c
Pamela Moreira <pamelamoreiras99@gmail.com>
2022-05-27 17:44:04 -0300

    implements métodos salvar e alterar na service e converter

--- src/main/java/com/pamela/biblioteca/api/repository/LivroRepository.java
+++ src/main/java/com/pamela/biblioteca/api/repository/LivroRepository.java
@@ -2,6 +2,8 @@ package com.pamela.biblioteca.api.repository;
 
 import com.pamela.biblioteca.api.entity.livros.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LivroRepository extends JpaRepository<Livro,Long> {
 }
